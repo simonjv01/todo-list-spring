3215bf84433b92d6a2fa94de0e1eb3eed27580f9
simonjv01 <sjvargas01@gmail.com>
2021-01-02 23:49:03 -0500

    updated WEB-INF files

---
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+
+    // http://localhost:8080/todo-list-spring/hello
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    // http://localhost:8080/todo-list-spring/welcome
+
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
+        return "welcome";
+    }
+}
